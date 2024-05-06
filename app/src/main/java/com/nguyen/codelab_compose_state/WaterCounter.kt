b027901de52102c10459272b34e538845195ae6c
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-05-06 15:58:21 -0700

    b4: Memory in a composable function

--- app/src/main/java/com/nguyen/codelab_compose_state/WaterCounter.kt
+++ app/src/main/java/com/nguyen/codelab_compose_state/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -16,7 +20,13 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     // * add some extra padding to the top of the Button to separate it from the Text
     // * change count from val to var so it becomes mutable
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        // MutableState: changes to count are now tracked by Compose
+        // A value calculated by remember is stored in the Composition during the initial
+        // composition, and the stored value is kept across recompositions
+        // Usually remember and mutableStateOf are used together in composable functions
+        // val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
