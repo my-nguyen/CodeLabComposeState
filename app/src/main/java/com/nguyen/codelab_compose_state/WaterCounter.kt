e0a9d1e81e2c92cc7d5ef79a8e294c39104f1a97
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-05-06 15:34:20 -0700

    b3: Events in Compose

--- app/src/main/java/com/nguyen/codelab_compose_state/WaterCounter.kt
+++ app/src/main/java/com/nguyen/codelab_compose_state/WaterCounter.kt
@@ -1,6 +1,8 @@
 package com.nguyen.codelab_compose_state
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,9 +10,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    // add the Button below the Text.
+    // * use a Column to vertically align the Text with the Button.
+    // * move the external padding from the Text to the Column.
+    // * add some extra padding to the top of the Button to separate it from the Text
+    // * change count from val to var so it becomes mutable
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
